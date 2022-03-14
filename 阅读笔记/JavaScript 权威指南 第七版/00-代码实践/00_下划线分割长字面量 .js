4e77959eb967ee7c02a2aabfb503da968034c83a
huaqi <2931246464@qq.com>
2022-03-14 15:46:43 +0800

    '阅读笔记JavaScript权威指南-number部分'

---
+++ 阅读笔记/JavaScript 权威指南 第七版/00-代码实践/00_下划线分割长字面量 .js
@@ -0,0 +1,11 @@
+let billion = 1_000_000_000; // Underscore as a thousands separator.
+let bytes = 0x89_ab_cd_ef; // As a bytes spearator.
+let bits = 0b0001_1101_0111; // As a nibble separator.
+let fraction = 0.123_456_789; // Works in the fractional part, too.
+
+console.log(billion);
+console.log(bytes);
+console.log(bits);
+console.log(fraction);
+
+// 注：于 chrome 浏览器可以实现 2022/03/14
