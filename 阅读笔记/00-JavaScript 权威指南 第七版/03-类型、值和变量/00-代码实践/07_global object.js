3402e94c6f502866221ab05796a179c87449921d
huaqi <2931246464@qq.com>
2022-03-20 16:52:20 +0800

    '添加阅读笔记-JavaScript权威指南-global-object部分'

---
+++ 阅读笔记/00-JavaScript 权威指南 第七版/03-类型、值和变量/00-代码实践/07_global object.js
@@ -0,0 +1,11 @@
+console.log("2022/03/20-Chrome/Node");
+console.log("globalThis: ");
+console.log(globalThis);
+console.log("===========");
+
+console.log("window: ");
+console.log(window);
+console.log("===========");
+
+console.log("global: ");
+console.log(global);
