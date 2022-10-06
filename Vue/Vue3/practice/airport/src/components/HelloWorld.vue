bad4c1a79dc14cee06de5a219a8487b4a152007e
huaqi <2931246464@qq.com>
2022-10-06 22:54:01 +0800

    feat：一些好玩的代码实践

---
+++ Vue/Vue3/practice/airport/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="airport-panel">
+        <div class="header">
+            机场信息
+            <div class="close-btn"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .header {
+        background: url(../assets/images/bg.png);
+    }
+</style>
