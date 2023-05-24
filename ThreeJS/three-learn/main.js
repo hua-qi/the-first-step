c0d378d4af336c070ffbf77bf0055efabc8c1b47
huaqi <2931246464@qq.com>
2023-05-24 23:55:56 +0800

    feat: three起步

--- ThreeJS/three-learn/main.js
+++ ThreeJS/three-learn/main.js
@@ -1,24 +1,10 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+/*
+ * @Description  : three 起步
+ * @Author       : lianzimeng
+ * @Date         : 2023-05-24 23:51:15
+ * @LastEditors  : lianzimeng
+ * @LastEditTime : 2023-05-24 23:54:57
+ */
+import * as THREE from 'three'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+console.log(THREE);
